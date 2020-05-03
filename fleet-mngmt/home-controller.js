3390484476a36092bbfffa43f8826596d161e593
filipskiba <filipskiba.247@gmail.com>
2020-05-03 22:14:57 +0200

    create float manager

--- fleet-mngmt/home-controller.js
+++ fleet-mngmt/home-controller.js
@@ -1,24 +1,13 @@
 $(document).ready(function () {
-    var apiRoot = 'https://pacific-castle-21497.herokuapp.com/api/rates';
-    var rates = [];
-    getRates();
+    var rates = ['MagBud','FilBud','FilMagBud'];
+    newsRotate(0);
 
-    function getRates() {
-        $.ajax({
-            url: apiRoot,
-            method: 'GET',
-            success: function (data) {
-                rates = data;
-                newsRotate(0);
-            }
-        });
-    }
 
     function newsRotate(i) {
 
         $("#newsbar").fadeOut(1000);
         $("#newsbar").promise().done(function () {
-            $("#newsbar").html(rates[i].currency + " " + rates[i].mid + "zł");
+            $("#newsbar").html(rates[i]);
             $("#newsbar").fadeIn(1000).delay(5000);
             if (i < rates.length - 1)
                 newsRotate(i + 1);
