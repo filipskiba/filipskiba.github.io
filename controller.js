579545c327356e8f4398b83a1ff6aa1f07a7641c
filipskiba <filipskiba.247@gmail.com>
2020-09-03 17:36:02 +0200

    affixed scrollspy

--- controller.js
+++ controller.js
@@ -2,21 +2,17 @@ $(document).ready(function () {
     // Get the current year for the copyright
     $('#year').text(new Date().getFullYear());
 
-    // Init Scrollspy
-    $('body').scrollspy({ target: '#main-nav' });
+    $('body').scrollspy({ target: '#main-nav', offset: 100 });
 
-    // Smooth Scrolling
+   //Smooth Scrolling and affix 100
     $("#main-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
 
+         if(this.hash !== ""){
             const hash = this.hash;
-
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top -100
             }, 800, function () {
-
-                window.location.hash = hash;
+                window.location.hash = hash-100;
             });
         }
     });
