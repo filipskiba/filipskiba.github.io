08a6ac310ffe53b1953821bc73a226d4f4981a7b
filipskiba <filipskiba.247@gmail.com>
2020-09-09 16:43:01 +0200

    upgrade homepage

--- controller.js
+++ controller.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
     $('#year').text(new Date().getFullYear());
 
 
-
-    var navOffset = 100;
    //Smooth Scrolling
     $("#main-nav a").on('click', function (event) {
          if(this.hash !== ""){
@@ -16,4 +14,26 @@ $(document).ready(function () {
             });
         }
     });
+
+    $('#downloadFile').click(function () {
+        downloadCv();
+    });
+
+    function downloadCv(){
+        var url = window.location.origin+'/filipskiba.github.io/CV.pdf';
+        var req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.responseType = "blob";
+
+        req.onload = function (event) {
+            var blob = req.response;
+            console.log(blob.size);
+            var link=document.createElement('a');
+            link.href=window.URL.createObjectURL(blob);
+            link.download="Filip Skiba CV.pdf";
+            link.click();
+        };
+
+        req.send();
+    }
 });
